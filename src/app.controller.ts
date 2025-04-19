dad9bd47131e5bac1e27404248df294dfdccf3c4
nabiAbdelkadir <71078581+nabiAbdelkadir@users.noreply.github.com>
2025-04-19 15:12:58 +0200

    feat(i18n): setup internationalization with initial config and dependencies - Added nestjs-i18n and configured JSON loader with query and header resolvers. - Created initial translation structure under /i18n.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { I18n, I18nContext } from 'nestjs-i18n';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  greet(@I18n() i18n: I18nContext) {
+    return i18n.t('common.greeting', {
+      args: { name: 'John' },
+    });
+  }
 
   @Get()
   getHello(): string {
